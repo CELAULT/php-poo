263d44bb9b19ff0dfaa8d0531d3305770bc5f8e2
Célien <71440955+CELAULT@users.noreply.github.com>
2021-09-09 11:08:28 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    include("personnage.php");
+    
+    print("<h1>Jeu de Combat</h1>");
+
+    $perso = new Personnage();
+    $perso -> parler();
+?>
--- personnage.php
+++ personnage.php
@@ -4,6 +4,9 @@
         private $_force = 50;
         private $_degats = 10;
         private $_experience = 0;
+
+        public function parler() {
+        print("Je suis un personnage."); }
     
         public function frapper() {}
 
