e15c5b012babdb9677ccac8e575761cf15fd9b77
Célien <71440955+CELAULT@users.noreply.github.com>
2021-09-09 11:51:52 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -14,15 +14,22 @@
             <?php
                 include "personnage.php";
 
-                print("<h1>Jeu de Combat</h1>");
+                print("<h1>Jeu de Combat</h1><br><br><br>");
 
                 $perso1 = new Personnage();
-                $perso2 = new Personnage();
+                $perso1 -> definirForce(20);
+                $perso1 -> definirExperience(15);
 
-                $perso1 -> parler();
-                $perso1 -> gagnerExperience();
+                $perso2 = new Personnage();
+                $perso2 -> definirForce(60);
+                $perso2 -> definirExperience(1);
 
                 $perso1 -> frapper($perso2);
+                $perso2 -> frapper($perso1);
+
+                print("Dégâts du J1 = " . $perso1 -> afficherDegats());
+                print("<br>Dégâts du J2 = " . $perso2 -> afficherDegats());
+
             ?>
 
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
--- personnage.php
+++ personnage.php
@@ -1,9 +1,25 @@
 <?php
     class Personnage {
         private $_nom = 'Inconnu';
-        private $_force = 50;
-        private $_degats = 10;
+        private $_force = 0;
+        private $_degats = 0;
         private $_experience = 0;
+
+        public function definirForce($force) {
+            $this -> _force = $force;
+        }
+
+        public function definirDegats($_degats) {
+            $this -> _degats = $_degats;
+        }
+
+        public function afficherDegats() {
+            return $this -> _degats;
+        }
+
+        public function definirExperience($_experience) {
+            $this -> _experience = $_experience;
+        }
 
         public function parler() {
             print("Je suis un personnage !");
@@ -11,10 +27,14 @@
 
         public function frapper($adversaire) {
             $adversaire -> _degats = $adversaire -> _degats + $this -> _force;
+            $this -> gagnerExperience();
         }
 
         public function gagnerExperience() {
             $this -> _experience++;
+        }
+
+        public function afficherExperience() {
             return $this -> _experience;
         }
     }
